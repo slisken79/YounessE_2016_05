5d7a079b81c41ae676cea3cc592aec741b69f049
slisken79 <essadak@yahoo.se>
2016-05-27 09:05:24 +0200

    Commit

---
+++ YounessE_MVC_2016_05/YounessE_MVC_2016_05/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YounessE_MVC_2016_05.Global" Language="C#" %>
---
+++ YounessE_MVC_2016_05/YounessE_MVC_2016_05/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(YounessE_MVC_2016_05.Startup))]
+namespace YounessE_MVC_2016_05
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
