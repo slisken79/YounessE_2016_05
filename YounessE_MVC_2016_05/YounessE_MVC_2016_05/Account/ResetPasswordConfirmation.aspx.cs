404bcc8f1a935b475958d1d8f87196cf598fe886
slisken79 <essadak@yahoo.se>
2016-05-27 11:21:40 +0200

    Yes

--- YounessE_MVC_2016_05/YounessE_MVC_2016_05/Account/ResetPasswordConfirmation.aspx.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Web.UI;
-
-namespace YounessE_MVC_2016_05.Account
-{
-    public partial class ResetPasswordConfirmation : Page
-    {
-    }
-}
--- YounessE_MVC_2016_05/YounessE_MVC_2016_05/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="YounessE_MVC_2016_05.Global" Language="C#" %>
